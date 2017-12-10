f7d50d246c296f78938410fbde166fda28f68d2d
flwkemd <flwkemd@naver.com>
2017-12-10 22:42:01 +0100

    initial commit

---
+++ src/com/ex/aop/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.ex.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.ex.aop")
+public class DemoConfig {
+
+}
